a0eef81052f5160f7432afa3c6eea3277c15fa64
fbrahman <fbrahman@gmail.com>
2017-05-27 09:57:27 -0700

    lots of changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 // firbase configuration
-
 var config = {
-  apiKey: "AIzaSyCHnAX4seXIKlOgNKk87sjZsjfTgrFks6E",
-  authDomain: "rps-multi-aa34c.firebaseapp.com",
-  databaseURL: "https://rps-multi-aa34c.firebaseio.com",
-  projectId: "rps-multi-aa34c",
-  storageBucket: "rps-multi-aa34c.appspot.com",
-  messagingSenderId: "609772832488"
+    apiKey: "AIzaSyCHnAX4seXIKlOgNKk87sjZsjfTgrFks6E",
+    authDomain: "rps-multi-aa34c.firebaseapp.com",
+    databaseURL: "https://rps-multi-aa34c.firebaseio.com",
+    projectId: "rps-multi-aa34c",
+    storageBucket: "rps-multi-aa34c.appspot.com",
+    messagingSenderId: "609772832488"
 };
 
 // intializing firebase
@@ -15,368 +14,288 @@ firebase.initializeApp(config);
 let database = firebase.database();
 let userRef = database.ref("/users");
 let gameRef = database.ref("/game");
+let chatRef = database.ref("/chat");
 
 // game script start
 
 let playerName = "";
 
-let userInfo  = {
-	name:"",
-	uKey:"",
-	role:""
+let userInfo = {
+    name: "",
+    uKey: "",
+    role: "",
+    cScore: 0,
+    jScore: 0,
+    tie: 0
 };
 
-let gameInfo ={
-	creator:"",
-	joiner:"",
-	gKey : "",
+let gameInfo = {
+    creator: "",
+    joiner: "",
+    gKey: "",
 }
 
 $("#nameModal").modal({
-	backdrop:"static", 
-	keyboard: true
+    backdrop: "static",
+    keyboard: true
 });
 
-$("#submitName").click(function(
-	){
-	playerName = $("#nameInput").val().trim();
+$("#submitName").click(function() {
+    playerName = $("#nameInput").val().trim();
 
-	let playerInfo = {
-		name:playerName,
-		key:""
-	};
+    let playerInfo = {
+        name: playerName,
+        key: ""
+    };
 
-	let key = userRef.push(playerInfo).key;
+    let key = userRef.push(playerInfo).key;
 
-	userRef.child(key).update({
-		key:key
-	});
+    userRef.child(key).update({
+        key: key
+    });
 
-	userInfo.name = playerName;
-	userInfo.uKey = key;
+    userInfo.name = playerName;
+    userInfo.uKey = key;
 
-	userRef.child(key).onDisconnect().remove();
+    userRef.child(key).onDisconnect().remove();
 
-	database.ref().on("value", function(snap){
-		if(!snap.child("game").exists()){
-			$("#createGameBtn").removeClass("disabled");
-			console.log("creator doesn't exist");
-		}else if (userInfo.role === ""){
-			$("#joinGameBtn").removeClass("disabled");
-			console.log("creator does exist");
-			console.log(snap.child("game").val());
-			let pullGame = snap.child("game").val();
-			let pullGameKeyArr = Object.keys(pullGame);
-			let lastIndex = pullGameKeyArr.length - 1;
-			let lastKey = pullGameKeyArr[lastIndex];
-			let lastObj = pullGame[lastKey];
+    database.ref().on("value", function(snap) {
+        if (!snap.child("game").exists()) {
+            $("#createGameBtn").removeClass("disabled");
+            // console.log("creator doesn't exist");
+        } else if (userInfo.role === "") {
+            $("#joinGameBtn").removeClass("disabled");
+            // console.log("creator does exist");
+            // console.log(snap.child("game").val());
+            let pullGame = snap.child("game").val();
+            let pullGameKeyArr = Object.keys(pullGame);
+            let lastIndex = pullGameKeyArr.length - 1;
+            let lastKey = pullGameKeyArr[lastIndex];
+            let lastObj = pullGame[lastKey];
 
-			// gameInfo.creator = lastObj.creator;
-			gameInfo.gKey = lastObj.gKey;
+            // gameInfo.creator = lastObj.creator;
+            gameInfo.gKey = lastObj.gKey;
 
-			$("#createGameBtn").addClass("disabled");
+            $("#createGameBtn").addClass("disabled");
 
-			// console.log(gameInfo.creator,gameInfo.gKey);
-		}
-	})
+            // console.log(gameInfo.creator,gameInfo.gKey);
+        }
+    })
 });
 
-$("#createGameBtn").click(function(){
-	let game = {
-		creator:playerName,
-		gKey:"",
-		state:1
-	};
+function createGame() {
+    $("#createGameBtn").click(function() {
+        let game = {
+            creator: playerName,
+            gKey: "",
+            state: 1
+        };
 
-	let key = gameRef.push(game).key;
-	gameRef.child(key).update({
-		gKey:key
-	});
+        let key = gameRef.push(game).key;
+        gameRef.child(key).update({
+            gKey: key
+        });
 
-	gameInfo.creator = playerName;
-	gameInfo.gKey = key;
+        gameInfo.creator = playerName;
+        gameInfo.gKey = key;
 
-	userRef.child(userInfo.uKey).update({
-		role:"creator"
-	})
+        userRef.child(userInfo.uKey).update({
+            role: "creator"
+        })
 
-	userInfo.role = "creator";
+        userInfo.role = "creator";
 
-	$("#createGameBtn").addClass("disabled");
-	$("#joinGameBtn").addClass("disabled");
+        $("#createGameBtn").addClass("disabled");
+        $("#joinGameBtn").addClass("disabled");
 
-	createGameBoard();
-});
-
-$("#joinGameBtn").click(function(){
-	gameRef.child(gameInfo.gKey).update({
-		joiner:playerName,
-		state: 2
-	})
-
-	gameInfo.joiner = playerName;
-	userRef.child(userInfo.uKey).update({
-		role:"joiner"
-	})
-
-	userInfo.role = "joiner";
-
-	$("#joinGameBtn").addClass("disabled");
-
-	createGameBoard();
-});
-
-function createGameBoard(){
-	
-	gameRef.child(gameInfo.gKey).on("value",function(snap){
-		let state = snap.val().state;
-		let p1 = snap.val().creator;
-		let p2 = snap.val().joiner||"waiting for player";
-		console.log(state);
-
-		switch (state) {
-			case 1:
-				$("#leftPlayer").removeClass("hidePlayer");
-				$("#cName").text(p1);
-				break;
-			case 2:
-				$("#rightPlayer").removeClass("hidePlayer");
-				$("#jName").text(p2);
-
-				$("#leftPlayer").removeClass("hidePlayer");
-				$("#cName").text(p1);
-
-				playerChoice();
-				break;
-		}
-
-	});
-
-	// if (caller === "creator"){
-	// 	console.log("creator called me - watiing for player");
-
-	// } else if( caller = "joiner"){
-
-	// gameRef.child(gameInfo.gKey).on("value",function(snap){ 
-	// 	let p1 = snap.val().creator;
-	// 	let p2 = snap.val().joiner;
-
-	// 	$("#rightPlayer").removeClass("hidePlayer");
-	// 	$("#jName").text(p2);
-
-	// 	$("#leftPlayer").removeClass("hidePlayer");
-	// 	$("#cName").text(p1);
-	// })
-	
-	//}
+        createGameBoard();
+    });
 }
 
-function playerChoice(){
+function joinGame() {
+    $("#joinGameBtn").click(function() {
+        gameRef.child(gameInfo.gKey).update({
+            joiner: playerName,
+            state: 2
+        })
 
-	if (userInfo.role === "creator"){
-		$(".lGameOption").click(function(){
-			let pChoice = ($(this).attr("value"));
-			gameRef.child(gameInfo.gKey).update({
-				creatorChoice:pChoice
-			})
-		})
-		winLose();
-	} else if (userInfo.role === "joiner") {
-		$(".rGameOption").click(function(){
-			let jChoice = ($(this).attr("value"));
-			gameRef.child(gameInfo.gKey).update({
-				joinerChoice:jChoice
-			})
-		})
-		winLose();
-	}
+        gameInfo.joiner = playerName;
+        userRef.child(userInfo.uKey).update({
+            role: "joiner"
+        })
+
+        userInfo.role = "joiner";
+
+        $("#joinGameBtn").addClass("disabled");
+
+        createGameBoard();
+    });
 }
 
-function winLose(){
-	gameRef.child(gameInfo.gKey).once("value", function(snap){
-		if(snap.child("creatorChoice").exists() && snap.child("joinerChoice").exists()){
-			let cChoice = snap.val().creatorChoice;
-			let jChoice = snap.val().joinerChoice;
-			let winState = (3 + cChoice - jChoice) % 3;
+userRef.on("child_removed", function(snap) {
+    chatRef.push().set({
+    	name: "Alert",
+    	message: "player disconnected"
+    });
 
-			console.log("inside if statement for winLose");
+    gameRef.child(gameInfo.gKey).remove();
 
-			switch(winState){
-				case 0:
-					console.log("it's a tie!");
-					break;
-				case 1:
-					console.log("Creator win's");
-					break;
-				case 2:
-					console.log("Joiner win's");
-					break;
-			}
+    $("#leftPlayer").addClass("hidePlayer");
+    $("#rightPlayer").addClass("hidePlayer");
+    $("#score").addClass("hideScore");
 
-		}
-	})
+
+    startGame();
+
+});
+
+function startGame() {
+    createGame();
+    joinGame();
+
+    $("#chatAreaName").empty();
+    $("#chatAreaMessage").empty();
 }
 
+startGame();
 
+function createGameBoard() {
 
+    $("#createGameBtn").off("click");
+    $("#joinGameBtn").off("click");
 
+    gameRef.child(gameInfo.gKey).on("value", function(snap) {
+        let state = snap.val().state;
+        let p1 = snap.val().creator;
+        let p2 = snap.val().joiner || "waiting for player";
+        console.log(state);
 
+        switch (state) {
+            case 1:
+                $("#leftPlayer").removeClass("hidePlayer");
+                $("#cName").text(p1);
+                break;
+            case 2:
+                $("#rightPlayer").removeClass("hidePlayer");
+                $("#jName").text(p2);
 
+                $("#leftPlayer").removeClass("hidePlayer");
+                $("#cName").text(p1);
 
+                $("#score").removeClass("hideScore");
+                $("#cNameS").text(p1);
+                $("#jNameS").text(p2);
 
-// var databaseFB = {
-// 	database: firebase.database(),
+                playerChoice();
+            case 3:
+                gameRef.child(gameInfo.gKey).off("value");
+                break;
+        }
 
-// 	connections: function (){
-		
-// 		let connectionsRef = databaseFB.database.ref("/connections");
-// 		let connectedRef = databaseFB.database.ref(".info/connected");
-		
-// 		connectedRef.on("value", function(snap){
-// 			if(snap.val()){
-// 				let con = connectionsRef.push(true);
-// 				con.onDisconnect().remove();
-// 			}
-// 		});
-// 	},
+    });
+}
 
-// 	createGame: function(player1){
+function playerChoice() {
 
-// 		let gameRef = databaseFB.database.ref("/games");
+    switch (userInfo.role) {
+        case "creator":
+            $(".lGameOption").off().click(function() {
+                let pChoice = ($(this).attr("value"));
+                gameRef.child(gameInfo.gKey).update({
+                    creatorChoice: pChoice
+                })
+                winLose();
+            });
+        case "joiner":
+            $(".rGameOption").off().click(function() {
+                let jChoice = ($(this).attr("value"));
+                gameRef.child(gameInfo.gKey).update({
+                    joinerChoice: jChoice
+                })
+                winLose();
+            });
+    }
+}
 
-// 		gamePlayer.player1.name = player1;
-// 		gamePlayer.state = 1;
-// 		gamePlayer.currGameKey = gameRef.push(gamePlayer).key;
+function scoreUpdate() {
+    gameRef.child(gameInfo.gKey).on("value", function(snap) {
+        let tScore = snap.val().tScore||0;
+        let cScore = snap.val().cScore||0;
+        let jScore = snap.val().jScore||0;
 
-// 		console.log(gamePlayer.currGameKey);
-// 	}, 
+        console.log("scores function",tScore,cScore,jScore)
 
-// 	joinGame: function(player2){
+        $("#tieScoreNum").text(tScore);
+        $("#creatorScoreNum").text(cScore);
+        $("#joinerScoreNum").text(jScore);
+    })
+}
 
-// 		let gameRef = databaseFB.database.ref("/games");
-// 		let gameRefUpdate = gameRef.child(gamePlayer.currGameKey);
+function winLose() {
+    console.log("I'm in the winLose function!");
+    gameRef.child(gameInfo.gKey).once("value", function(snap) {
+        if (snap.child("creatorChoice").exists() && snap.child("joinerChoice").exists()) {
+            let cChoice = snap.val().creatorChoice;
+            let jChoice = snap.val().joinerChoice;
+            let winState = (3 + cChoice - jChoice) % 3;
+            let cScore = snap.val().cScore || 0;
+            let jScore = snap.val().jScore || 0;
+            let tScore = snap.val().tScore || 0;
 
-// 		gameRefUpdate.update(gamePlayer);
-// 		gameRefUpdate.transaction(function(test){
+            console.log("scores", cScore, jScore, tScore);
 
-// 			if(test.state === 1){
+            console.log("inside if statement for winLose");
 
-// 				gamePlayer.state = 2;
-// 				test.state = gamePlayer.state;
-// 				gamePlayer.player2.name = player2;
-// 				test.player2.name = gamePlayer.player2.name;
-// 			}
-// 			return test;
+            scoreUpdate();
 
-// 		})
-// 	}
-// };
+            switch (winState) {
+                case 0:
+                    console.log("it's a tie!");
+                    tScore++;
+                    gameRef.child(gameInfo.gKey).update({
+                            tScore: tScore
+                        })
+                    break;
+                case 1:
+                    console.log("Creator win's");
+                    cScore++;
+                    gameRef.child(gameInfo.gKey).update({
+                            cScore: cScore
+                        })
+                    break;
+                case 2:
+                    console.log("Joiner win's");
+                    jScore++;
+                    gameRef.child(gameInfo.gKey).update({
+                            jScore: jScore
+                        })
+                    break;
+            }
+            // $(".lGameOption").off("click");
+            // $(".rGameOption").off("click");
+            gameRef.child(gameInfo.gKey).update({ state: 3 })
+            console.log("I'm after the switch statment!")
+        }
+    })
+}
 
-// var gamePlayer = {
-// 	currGameKey: "",
-// 	state: 0,
-// 	player1: {
-// 		name:"",
-// 		choice:"",
-// 		player1key:"",
-// 		win: 0, 
-// 		lose: 0,
-// 		tie: 0,
-// 	},
-// 	player2: {
-// 		name:"",
-// 		choice:"",
-// 		player2key:"",
-// 		win: 0, 
-// 		lose: 0,
-// 		tie: 0,
-// 	},
-// };
+function sendChat() {
+    messageField = $("#chatMsg").val();
 
-// databaseFB.connections();
+    chatRef.push().set({
+        name: userInfo.name,
+        message: messageField
+    });
+}
 
-// $("#createGame").click(function(){
-	
-// 	let playerName = $("#playerName").val();
+function addChatMessage(name, message) {
+    $("#chatAreaName").append("<br>" + "<strong>" + name + ":</strong> ");
+    $("#chatAreaMessage").append("<br>" + message);
+}
 
-// 	if (gamePlayer.state === 0){
-// 		databaseFB.createGame(playerName);
-// 	} else if (gamePlayer.state === 1){
-// 		databaseFB.joinGame(playerName);
-// 	}
+chatRef.on("child_added", function(snap) {
+    let message = snap.val();
+    addChatMessage(message.name, message.message);
+})
 
-// });
-
-// var test = "";
-
-// databaseFB.database.ref().on("value", function(snapshot) {
-
-// 	if (snapshot.child("games").exists()){
-// 		let game = snapshot.val().games;
-// 		test = game;
-
-// 		console.log("inside the loop - games exists")
-
-// 		// gamePlayer.currGameKey = game.currGameKey;
-// 		// gamePlayer.state = game.state;
-// 		// game.player1.name = game.player1.name;
-// 	}
-// });
-
-
-
-
-
-
-// var rps = {
-//     player1: {
-//         name: "",
-//         choice: "",
-//         win: 0,
-//         lose: 0
-//     },
-//     player2: {
-//         name: "",
-//         choice: "",
-//         win: 0,
-//         lose: 0
-//     },
-//     tie: 0,
-
-//     winLose: function(choice1, choice2) {
-//         if (choice1 === choice2) {
-//             tie++;
-//         } else if (choice1 === "p") {
-//             if (choice2 === "r") {
-//                 this.player1.win++;
-//                 this.player2.lose++;
-//             } else {
-//                 this.player2.win++;
-//                 this.player1.lose++;
-//             }
-//         } else if (choice1 === "r") {
-//             if (choice2 === "s") {
-//                 this.player1.win++;
-//                 this.player2.lose++;
-//             } else {
-//                 this.player2.win++;
-//                 this.player1.lose++;
-//             }
-//         } else if (choice1 === "s") {
-//             if (choice2 === "p") {
-//                 this.player1.win++;
-//                 this.player2.lose++;
-//             } else {
-//                 this.player2.win++;
-//                 this.player1.lose++;
-//             }
-//         }
-//     },
-
-//     player: function(name, choice, win, lose) {
-//         this.name = name;
-//         this.choice = choice;
-//         this.win = win;
-//         this.lose = lose;
-//     }
-// }
+$("#chatBtn").click(sendChat);
